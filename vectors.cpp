668095a87ef7ed4e4b9f377ecabc55906bc54438
Malya12 <malyasri0112@gmail.com>
2023-10-11 23:55:52 +0530

    code

---
+++ vectors.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[10]; //fixed size
+    vector<int>v; //size can be modified
+    int n;
+    cin>>n;
+    for(int i;i<n;++i){
+        int x;
+        cin>>x;
+        v.push_back(x);  //O(1)
+        
+    }
+
+}

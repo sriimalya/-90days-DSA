e8c9896fe7ef7e3fb19ffaa88e3fbdabc16eda3b
Malya12 <malyasri0112@gmail.com>
2023-10-11 23:59:24 +0530

    code

--- vectors.cpp
+++ vectors.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void printVector(vector<int> v){
+    for(int i=0;i<v.size();++i){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
 int main(){
     int a[10]; //fixed size
     vector<int>v; //size can be modified
@@ -10,7 +16,6 @@ int main(){
         int x;
         cin>>x;
         v.push_back(x);  //O(1)
-        
     }
-
+    printVector(v);
 }
